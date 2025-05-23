d4695895d71f37abbfee0934c6ad9bf9a1883b5c
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-05-24 03:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+  const d1 = new Date(date1);
+  const d2 = new Date(date2);
+
+  const utc1 = Date.UTC(d1.getFullYear(), d1.getMonth(), d1.getDate());
+  const utc2 = Date.UTC(d2.getFullYear(), d2.getMonth(), d2.getDate());
+
+  const msPerDay = 1000 * 60 * 60 * 24;
+
+  return (utc2 - utc1) / msPerDay;
 };
 
 // Do not change the code below.
